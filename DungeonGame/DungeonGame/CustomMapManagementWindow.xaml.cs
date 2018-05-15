52734daf879ddf546fb3d3e45b6c3628542ebc34
Cajova-Houba <tkmushroomer@gmail.com>
2018-05-15 14:02:45 +0200

    #30: Added map management window.

---
+++ DungeonGame/DungeonGame/CustomMapManagementWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DungeonGame
+{
+    /// <summary>
+    /// Window used to manage custom maps.
+    /// </summary>
+    public partial class CustomMapManagementWindow : Window
+    {
+        public CustomMapManagementWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Display menu. Does not close this window.
+        /// </summary>
+        private void DisplayMenu()
+        {
+            MenuWindow menuWindow = new MenuWindow();
+            App.Current.MainWindow = menuWindow;
+            menuWindow.Show();
+        }
+
+        private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisplayMenu();
+        }
+    }
+}
